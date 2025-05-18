1b36dbe319b60646a24bb4f41eac3d5332c7b191
Tyler Johnson <tyler.tony.johnson@gmail.com>
2025-05-18 03:52:04 -0700

    MVP

--- src/lib/Scene.svelte
+++ src/lib/Scene.svelte
@@ -10,12 +10,22 @@
 		CatmullRomCurve3,
 		QuadraticBezierCurve3,
 		CurvePath,
-		LineCurve3
+		LineCurve3,
+		TubeGeometry,
+		DoubleSide
 	} from 'three';
 
 	// interactivity();
 	const PI = Math.PI;
 	const HALF_PI = PI / 2;
+	const directions = [
+		{ x: 1, y: 0, z: 0 },
+		{ x: 0, y: 1, z: 0 },
+		{ x: 0, y: 0, z: 1 },
+		{ x: -1, y: 0, z: 0 },
+		{ x: 0, y: -1, z: 0 },
+		{ x: 0, y: 0, z: -1 }
+	];
 
 	// --- CONFIG ---
 
@@ -29,13 +39,15 @@
 
 	let MAX_ANGLE_DEVIATION = $state((PI * 3) / 4);
 
-	let MAX_LENGTH_TOTAL = $state(100);
-	let STRAIGHT_BIAS = $state(0.5);
+	let MAX_LENGTH_TOTAL = $state(1600);
+	let STRAIGHT_BIAS = $state(1);
 	// const BEND_ARC = PI / 2;
 	// const BEND_RESOLUTION = 8;
 	let POINTS_PER_LENGTH = $state(2);
 
-	let growSpeed = $state(40); // units per second
+	let SPHERE_CHANCE = $state(0);
+
+	let growSpeed = $state(200); // units per second
 
 	//  --- GENERATION ---
 
@@ -43,41 +55,52 @@
 
 	// --- RUNTIME ---
 
-	let showPipes = $state(false);
+	let showPipes = $state(true);
 	let showBends = $state(false);
-	let showDebug = $state(true);
+	let showDebug = $state(false);
 
-	let pipes = $state();
-
-	const directions = [
-		{ x: 1, y: 0, z: 0 },
-		{ x: 0, y: 1, z: 0 },
-		{ x: 0, y: 0, z: 1 },
-		{ x: -1, y: 0, z: 0 },
-		{ x: 0, y: -1, z: 0 },
-		{ x: 0, y: 0, z: -1 }
-	];
-
-	let isPlaying = $state(false);
+	let isPlaying = $state(true);
 
 	let elapsed = $state(1);
-	let progress = $state(1);
-	// $inspect(progress);
+	let progress = $state(0.5);
+	// $inspect('progress', progress);
 
-	let curvePoints = $derived(getCurvePoints());
-	// $inspect(curvePoints);
+	// --- DATA ---
+	let pipes = $state();
+	let spheres = $state();
+	// $inspect('pipes', pipes);
+
+	let curvePath = $derived(getCurvePath(pipes));
+	// $inspect('path', curvePath);
+
+	let curvePoints = $derived(getCurvePoints(curvePath));
+	// $inspect('curve', curvePoints);
+
+	let progressPoints = $derived(getProgressPoints(curvePoints, progress));
+	// $inspect('progress', progressPoints);
+
+	let progressCurve = $derived(getProgressCurve(curvePath, progress));
+	// $inspect('progressCurve', progressCurve);
+
+	let isReady = $derived(
+		!!pipes && !!curvePath && !!curvePoints && !!progressPoints && !!progressCurve
+	);
+	// $inspect('ready', isReady);
+
+	let color = $state(getRandomColor());
 
 	// --- FUNCTIONS ---
 
 	function generatePipes() {
 		// Generate an array of points at right angles, random length
-		const points = [];
+		const newPipes = [];
+		const newSpheres = [];
 		let remainingLength = MAX_LENGTH_TOTAL;
 		let skipCount = 0;
 
 		// First point is origin
 		const firstPoint = { x: 0, y: 0, z: 0 };
-		points.push(firstPoint);
+		newPipes.push(firstPoint);
 
 		let i = 1;
 
@@ -89,22 +112,40 @@
 			}
 
 			// Choose a valid direction
-			let direction;
+			let direction, prevDirection;
 
-			if (points[i - 2]) {
-				const prevDirection = {
-					x: Math.sign(points[i - 2].x - points[i - 1].x),
-					y: Math.sign(points[i - 2].y - points[i - 1].y),
-					z: Math.sign(points[i - 2].z - points[i - 1].z)
+			let isStraight;
+
+			if (newPipes[i - 2]) {
+				prevDirection = {
+					x: Math.sign(newPipes[i - 2].x - newPipes[i - 1].x),
+					y: Math.sign(newPipes[i - 2].y - newPipes[i - 1].y),
+					z: Math.sign(newPipes[i - 2].z - newPipes[i - 1].z)
 				};
 
-				const filteredDirections = [...directions.filter(
-					(d) => !(d.x === prevDirection.x && d.y === prevDirection.y && d.z === prevDirection.z)
-				), ...Array(Math.ceil(STRAIGHT_BIAS * STRAIGHT_BIAS * 10)).fill({ x: -prevDirection.x, y: -prevDirection.y, z: -prevDirection.z })];
+				const filteredDirections = [
+					...directions.filter(
+						(d) => !(d.x === prevDirection.x && d.y === prevDirection.y && d.z === prevDirection.z)
+					),
+					...Array(Math.ceil(STRAIGHT_BIAS * STRAIGHT_BIAS * 10)).fill({
+						x: -prevDirection.x,
+						y: -prevDirection.y,
+						z: -prevDirection.z
+					})
+				];
 
 				direction = filteredDirections[Math.floor(Math.random() * filteredDirections.length)];
 			} else {
 				direction = directions[Math.floor(Math.random() * directions.length)];
+			}
+
+			// Check if the direction is straight
+			if (
+				prevDirection?.x === -direction.x &&
+				prevDirection?.y === -direction.y &&
+				prevDirection?.z === -direction.z
+			) {
+				isStraight = true;
 			}
 
 			// Get a valid length
@@ -112,20 +153,20 @@
 
 			// Generate coordinates based on the direction and length
 			const point = {
-				x: points[i - 1].x + direction.x * length,
-				y: points[i - 1].y + direction.y * length,
-				z: points[i - 1].z + direction.z * length
+				x: newPipes[i - 1].x + direction.x * length,
+				y: newPipes[i - 1].y + direction.y * length,
+				z: newPipes[i - 1].z + direction.z * length
 			};
 
 			// Skip if this point was already used
-			if (points.some((p) => p.x === point.x && p.y === point.y && p.z === point.z)) {
+			if (newPipes.some((p) => p.x === point.x && p.y === point.y && p.z === point.z)) {
 				// console.log('skipping point', point);
 				skipCount++;
 				continue;
 			}
 
 			// Calculate length since last point
-			const lastPoint = points[i - 1];
+			const lastPoint = newPipes[i - 1];
 			const lastLength = Math.hypot(
 				point.x - lastPoint.x,
 				point.y - lastPoint.y,
@@ -139,17 +180,30 @@
 			} else {
 				remainingLength -= lastLength;
 				// console.log('adding point', point);
-				points.push(point);
+				newPipes.push(point);
+
+				// Check if we should add a sphere
+				if (!isStraight && Math.random() < SPHERE_CHANCE) {
+					newSpheres.push(point);
+				}
 				i++;
 			}
 		}
 
-		pipes = points;
+		progress = 0;
+		color = getRandomColor();
+
+		pipes = newPipes;
+		spheres = newSpheres;
+		// curvePath = getCurvePath(pipes);
+		// [curvePoints, progressPoints] = getCurvePoints(curvePath);
 	}
 
-	function getCurvePoints() {
+	function getCurvePath(pipes) {
+		if (!pipes) return null;
+
 		// Create new path
-		const curvePath = new CurvePath();
+		const newCurvePath = new CurvePath();
 
 		// Create curves
 		for (let i = 0; i < pipes.length; i++) {
@@ -172,7 +226,7 @@
 
 				// Get spline points
 				const bezierCurve = new QuadraticBezierCurve3(...vertices);
-				curvePath.add(bezierCurve);
+				newCurvePath.add(bezierCurve);
 			}
 
 			if (i > 0) {
@@ -180,9 +234,15 @@
 				const offsetPoints = getPipePoints(i);
 				const lineCurve = new LineCurve3(...offsetPoints);
 				// console.log(lineCurve);
-				curvePath.add(lineCurve);
+				newCurvePath.add(lineCurve);
 			}
 		}
+
+		return newCurvePath;
+	}
+
+	function getCurvePoints(curvePath) {
+		if (!curvePath) return [];
 
 		// const points = curvePath.getPoints(4);
 		const length = curvePath.getLength();
@@ -191,19 +251,55 @@
 
 		const points = curvePath.getSpacedPoints(totalPointCount);
 
-		const progressPoints = points.slice(0, progress * totalPointCount);
+		// Format total points for buffer geometry
 
-		const floatArray = new Float32Array(progressPoints.length * 3);
+		const pointsArray = new Float32Array(points.length * 3);
 
-		progressPoints.forEach((point, index) => {
-			floatArray[index * 3] = point.x;
-			floatArray[index * 3 + 1] = point.y;
-			floatArray[index * 3 + 2] = point.z;
+		points.forEach((point, index) => {
+			pointsArray[index * 3] = point.x;
+			pointsArray[index * 3 + 1] = point.y;
+			pointsArray[index * 3 + 2] = point.z;
 		});
 
-		// Convert to array of coordinates
+		return pointsArray;
+	}
 
-		return floatArray;
+	function getProgressPoints(curvePoints, progress) {
+		if (!curvePoints) return [];
+
+		const progressPoints = curvePoints.slice(
+			0,
+			Math.floor(Math.floor(curvePoints.length * progress) / 3) * 3
+		);
+
+		return progressPoints;
+	}
+
+	function getProgressCurve(curvePath, progress) {
+		if (!curvePath) return null;
+
+		const progressCurve = curvePath.clone();
+
+		// Get the index of the cumulative length that is closest to the progress
+		const totalLength = curvePath.getLength();
+		const segmentLengths = curvePath.getLengths();
+		// console.table('segmentLengths', segmentLengths);
+
+		const targetLength = totalLength * progress;
+
+		// console.log('targetLength', targetLength);
+
+		progressCurve.curves = progressCurve.curves.filter((curve, index) => {
+			const isAccepted = segmentLengths[index + 1] < targetLength;
+			// console.table(index, segmentLengths[index], targetLength, isAccepted);
+
+			return isAccepted;
+		});
+
+		// console.log('segmentLengths', segmentLengths);
+		// console.log('progressCurve', progressCurve.curves);
+
+		return progressCurve;
 	}
 
 	function getPipePoints(index) {
@@ -247,6 +343,15 @@
 		return [insetStart, insetEnd];
 	}
 
+	function getRandomColor() {
+		const letters = '0123456789ABCDEF';
+		let color = '#';
+		for (let i = 0; i < 6; i++) {
+			color += letters[Math.floor(Math.random() * 16)];
+		}
+		return color;
+	}
+
 	function togglePlay() {
 		isPlaying = !isPlaying;
 	}
@@ -254,11 +359,20 @@
 	useTask((delta) => {
 		// Add elapsed time
 		if (!isPlaying) return;
+
 		elapsed += delta;
 
 		// Increment progress at the progress rate
 
-		progress = (progress + (delta * growSpeed) / MAX_LENGTH_TOTAL) % 1;
+		const newProgress = progress + (delta * growSpeed) / curvePath.getLength();
+		if (newProgress >= 1) {
+			generatePipes();
+			progress = 0;
+		} else {
+			progress = newProgress;
+		}
+
+		// progress = (progress + (delta * growSpeed) / MAX_LENGTH_TOTAL) % 1;
 
 		// console.log(delta);
 	});
@@ -269,50 +383,82 @@
 <!-- CAMERA -->
 <T.PerspectiveCamera
 	makeDefault
-	position={[20, 20, 20]}
+	position={[20, 10, 20]}
+	fov={90}
 	oncreate={(ref) => {
 		ref.lookAt(0, 1, 0);
 	}}
 >
-	<OrbitControls>
+	<!-- <OrbitControls>
 		<Gizmo />
-	</OrbitControls>
+	</OrbitControls> -->
 </T.PerspectiveCamera>
 
 <!-- LIGHTING -->
 <T.DirectionalLight position={[10, 10, 10]} intensity={10} />
 <T.AmbientLight color="white" intensity={1.45} />
 
-<!-- DEBUG VISUALS -->
+{#if isReady}
+	<!-- PIPE -->
+	{#if showPipes && progressCurve.curves.length > 0}
+		<T.Mesh>
+			<T.TubeGeometry
+				args={[progressCurve, progressPoints.length, PIPE_RADIUS, PIPE_SEGMENTS, false]}
+			/>
+			<T.MeshStandardMaterial {color} side={DoubleSide} />
+		</T.Mesh>
 
-{#if showDebug}
-	<!-- Dashed Line -->
-	<T.Points>
-		<T.BufferGeometry>
-			<T.BufferAttribute
-				args={[getCurvePoints(), 3]}
-				attach={({ parent, ref }) => {
-					parent.setAttribute('position', ref);
-				}}
-			></T.BufferAttribute>} />
-		</T.BufferGeometry>
-		<T.PointsMaterial size={0.25} />
-	</T.Points>
+		{#each spheres as sphere}
+			<T.Mesh position={(({ x, y, z }) => [x, y, z])(sphere)}>
+				<T.SphereGeometry args={[1.75, 8, 8]} />
+				<T.MeshStandardMaterial color="orange" />
+			</T.Mesh>
+		{/each}
+	{/if}
 
-	<!-- ORBS -->
-	<T.Mesh position={(({ x, y, z }) => [x, y, z])(pipes[0])}>
-		<T.SphereGeometry />
-		<T.MeshStandardMaterial color="orange" />
-	</T.Mesh>
-	<T.Mesh position={(({ x, y, z }) => [x, y, z])(pipes.at(-1))}>
-		<T.SphereGeometry />
-		<T.MeshStandardMaterial color="teal" />
-	</T.Mesh>
+	<!-- DEBUG VISUALS -->
+
+	{#if showDebug}
+		<!-- Total Path -->
+		<T.Points>
+			<T.BufferGeometry>
+				<T.BufferAttribute
+					args={[curvePoints, 3]}
+					attach={({ parent, ref }) => {
+						parent.setAttribute('position', ref);
+					}}
+				></T.BufferAttribute>} />
+			</T.BufferGeometry>
+			<T.PointsMaterial size={0.25} color="white" />
+		</T.Points>
+		<!-- Progress Path -->
+		<T.Points>
+			<T.BufferGeometry>
+				<T.BufferAttribute
+					args={[progressPoints, 3]}
+					attach={({ parent, ref }) => {
+						parent.setAttribute('position', ref);
+					}}
+				></T.BufferAttribute>} />
+			</T.BufferGeometry>
+			<T.PointsMaterial size={0.25} color="red" />
+		</T.Points>
+
+		<!-- ORBS -->
+		<T.Mesh position={(({ x, y, z }) => [x, y, z])(pipes[0])}>
+			<T.SphereGeometry />
+			<T.MeshStandardMaterial color="orange" />
+		</T.Mesh>
+		<T.Mesh position={(({ x, y, z }) => [x, y, z])(pipes.at(-1))}>
+			<T.SphereGeometry />
+			<T.MeshStandardMaterial color="teal" />
+		</T.Mesh>
+	{/if}
 {/if}
 
 <!-- TWEAKS -->
 
-<Pane position="fixed">
+<!-- <Pane position="fixed">
 	<Folder title="Generate">
 		<Checkbox bind:value={isOrtho} label="Orthogonal" />
 		<Stepper bind:value={MAX_LENGTH_TOTAL} min={20} max={10000} step={20} label="Length" />
@@ -320,7 +466,11 @@
 		<Button on:click={() => generatePipes()} label="Points" title="GENERATE" />
 	</Folder>
 
-	<Slider bind:value={BEND_RADIUS} min={0} max={10} label="Radius" />
+	<Folder title="Config">
+		<Checkbox bind:value={showPipes} label="Show Pipes" />
+		<Slider bind:value={BEND_RADIUS} min={0} max={10} label="Radius" />
+		<Slider bind:value={SPHERE_CHANCE} min={0} max={1} label="Orbs Chance" />
+	</Folder>
 
 	<Checkbox bind:value={showDebug} label="Debug" />
 	<Folder title="Animation">
@@ -328,4 +478,4 @@
 		<Slider bind:value={progress} min={0} max={1} label="Progress" />
 		<Button on:click={togglePlay} title={isPlaying ? 'Pause' : 'Play'} />
 	</Folder>
-</Pane>
+</Pane> -->
